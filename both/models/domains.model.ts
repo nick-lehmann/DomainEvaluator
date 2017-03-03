b777814e1f15648cb81eae15a7ae02d04d2bb4f9
Nick Lehmann <nickmann97@gmail.com>
2017-03-03 12:05:57 +0100

    Add id entry to Domain model

--- both/models/domains.model.ts
+++ both/models/domains.model.ts
@@ -1,24 +1,25 @@
 export interface Domain {
-  domain: string;
-  preSLD: string;
-  postSLD: string;
+    _id: string;
+    domain: string;
+    preSLD: string;
+    postSLD: string;
 
-  length: number;
-  hyphen: number;
-  words: number;
-  age: number;
-  year: number;
-  
-  visitors: number; // intervall-transformed
-  deGooglePreSLD: number;
-  enGooglePreSLD: number;
-  preMDLS: number;
-  postMDLS: number;
-  preCPC: number;
-  postCPC: number;
-  preCOMP: number;
-  HDAX: number;
-  googleAge: number;
-  wordsHyphen: number;
-  price: number;
+    length: number;
+    hyphen: number;
+    words: number;
+    age: number;
+    year: number;
+
+    visitors: number; // intervall-transformed
+    deGooglePreSLD: number;
+    enGooglePreSLD: number;
+    preMDLS: number;
+    postMDLS: number;
+    preCPC: number;
+    postCPC: number;
+    preCOMP: number;
+    HDAX: number;
+    googleAge: number;
+    wordsHyphen: number;
+    price: number;
 }
