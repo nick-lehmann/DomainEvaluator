3704fe4c820489348126c2c27dac5f5283c06abb
Nick Lehmann <nickmann97@gmail.com>
2017-03-05 23:20:12 +0100

    Adds finance api

---
+++ server/api/finance.api.ts
@@ -0,0 +1,57 @@
+import { DomainsCollection } from '../../both/collections/domains.collection';
+import { Domain } from '../../both/models/domains.model';
+import { HTTP } from 'meteor/http';
+
+cheerio = require('cheerio');
+
+class FinanceAPI {
+    constructor () {}
+
+    getDomain(domainID) {
+        // check if parameter is an id
+        var domain: Domain = <Domain>DomainsCollection.findOne({
+            _id: domainID
+        });
+        if (domain == undefined ) {
+            return false;
+        } else {
+            return domain;
+        }
+    }
+
+    getHDAX () {
+
+        var res = HTTP.call('GET', 'http://www.quotenet.com/index/HDAX');
+
+        if ( res.statusCode == 200 ) {
+            var $ = cheerio.load(res.content);
+            var hdaxRaw = $('.pricebox .content').find('th')[0].children[0].data
+            var hdax = parseFloat(hdaxRaw.replace(',', ''));
+            return hdax;
+        } else {
+            console.error('Request to Finance api failed');
+            return false;
+        }
+
+    }
+
+    fetchHDAX ( domainID ) {
+
+        var domain: Domain = this.getDomain(domainID);
+        var hdax = this.getHDAX();
+
+        if ( hdax ) {
+            DomainsCollection.update({
+                _id: domain._id
+            }, {
+                $set: {
+                    HDAX: hdax
+                }
+            });
+            return true;
+        }
+        return false;
+    }
+}
+
+export default new FinanceAPI();
