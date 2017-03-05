b9b5a0b1ea63dc10a8f46aae4d68eb40194da42b
Nick Lehmann <nickmann97@gmail.com>
2017-03-05 23:19:47 +0100

    Add api functionality for google search results

--- server/api/google.api.ts
+++ server/api/google.api.ts
@@ -38,7 +38,43 @@ class GoogleAPI {
 
     }
 
-    googleTrafficData(domainID) {
+    fetchSearchResults(domainID) {
+
+        var domain: Domain = this.getDomain(domainID);
+
+        // german preSLD
+        var queryGerman = "http://www.google.com/search?q=" + domain.preSLD + "&lr=lang_de";
+
+        console.log("German query: ", queryGerman);
+
+        var res = HTTP.call('GET', queryGerman);
+        console.log(res);
+        var $ = cheerio.load(res.content);
+        var germanParts = $('#resultStats').text().split(' ');
+        var germanResults = parseInt(germanParts[1].replace('.', '')); // in German, the single dot is used as decimal separator
+
+        // english preSLD
+        var queryEnglish = "http://www.google.com/search?q=" + domain.preSLD + "&lr=lang_en&hl=lang_en";
+        var res = HTTP.call('GET', queryEnglish);
+        var $ = cheerio.load(res.content);
+        var englishParts = $('#resultStats').text().split(' ');
+        var englishResults = parseInt(englishParts[1].replace('.', ''));
+
+        if ( germanResults && englishResults ) {
+            DomainsCollection.update({
+                _id: domain._id
+            }, {
+                $set: {
+                    deGooglePreSLD: germanResults,
+                    enGooglePreSLD: englishResults
+                }
+            });
+            return true;
+        } else {
+            console.error('Request to Google Web Search failed');
+            return false;
+        }
+    }
 
         var domain: Domain = this.getDomain(domainID);
 
